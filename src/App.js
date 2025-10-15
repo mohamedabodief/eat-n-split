ee4f014a0a57e880b999fc673fd1d92becf17eee
mohamed abodief <mohamed.abodeaf@gmail.com>
2025-10-15 09:04:23 +0300

    use a key prop for reset the state

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ export default function App() {
         <SplitBillForm
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
